a2acbc3b121b177714c3d32db49fafe27f95ee50
ajayrathod <rathodajay1202@gmail.com>
2022-07-20 16:33:55 +0530

    Puppeteer Deno Almost Completed

--- screenshot.js
+++ screenshot.js
@@ -1,4 +1,4 @@
-import puppeteer from "https://deno.land/x/puppeteer@14.1.1/mod.ts";
+import puppeteer from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
 import {
     Application,
     Router
@@ -8,6 +8,8 @@ import {
     viewEngine,
     denjuckEngine,
   } from "https://deno.land/x/view_engine@v10.5.1/mod.ts";
+// import * as mod from "https://deno.land/std@0.148.0/node/fs.ts";
+import { createWriteStream } from "https://deno.land/std@0.148.0/node/fs.ts";
 
 
 const router = new Router()
@@ -20,7 +22,7 @@ app.use(viewEngine(oakAdapter,denjuckEngine))
 
 
 router.get('/', (ctx) => {
-    ctx.render('index.html',{data:{name:"Hello Ajay"}})
+    ctx.render('public/index.html',{data:{name:"Hello Ajay"}})
     
 })
 
@@ -30,15 +32,17 @@ router.all('/screenshot',async(ctx) => {
     const value = await body.value.read();
     const browser = await puppeteer.launch()
     const page = await browser.newPage();
-    await page.goto(value.fields.url);
-    const screenshot = await page.screenshot({ path: "example.png" });
+    await page.goto(value.fields.url,{
+      waitUntil:'networkidle2'
+    });
+    const screenshot = await page.screenshot({ path: "public/example.png" });
     
     await browser.close();
     const blob = new Blob(screenshot,{type: 'image/png'});
 
     const imageURL = URL.createObjectURL(blob)
     // const downloadURL = Deno.cwd() + "public/example.png"
-    ctx.render('screenshot.html',{data:{src:imageURL}})
+    ctx.render('public/screenshot.html',{data:{src:"/example.png"}})
     ctx.response.status = 200
     
 })
@@ -48,40 +52,40 @@ router.all('/pdf',async(ctx) => {
   if(ctx.request.method == "POST"){
     const body = ctx.request.body({type: 'form-data'})
     const value = await body.value.read();
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({product:"chrome"})
     const page = await browser.newPage();
     await page.goto(value.fields.url,{
       waitUntil: 'networkidle2',
     });
-    const pdf = await page.pdf({path:"example.pdf",format:"A4"});
+    
+    await page.pdf({path:"public/example.pdf",format:"a4"});
+    // Stream a PDF into a file
+    // const pdfStream = await page.createPDFStream();
+    // const writeStream = createWriteStream('public/example.pdf');
+    // pdfStream.pipeThrough(writeStream);
+    // console.log("blocked");
     
     await browser.close();
-    const headers = new Headers()
+    // const headers = new Headers()
     // const downloadURL = Deno.cwd() + "public/example.png"
-    headers.append('Content-Length', String(pdf.byteLength))
-    headers.append('Content-Type','application/pdf')
-    headers.append('Content-Disposition', 'attachment; filename=quote.pdf')
-    ctx.response.body = pdf
-    ctx.response.headers = headers
+    // headers.append('Content-Length', String(pdf.byteLength))
+    // headers.append('Content-Type','application/pdf')
+    // headers.append('Content-Disposition', 'attachment; filename=quote.pdf')
+    ctx.render('public/pdf.html',{data:{src:"/example.pdf"}})
     ctx.response.status = 200
 
   }
-  if(ctx.request.method == "GET"){
-    ctx.render('pdf.html')
-
-  }
-  
 })
 
 app.use(router.routes());
 app.use(router.allowedMethods())
 
 
-// app.use(async (context) => {
-//   await context.send({
-//       root: `${Deno.cwd()}/public`,
-//   })
-// });
+app.use(async (context) => {
+  await context.send({
+      root: `${Deno.cwd()}/public`,
+  })
+});
 
 app.addEventListener("listen",() => {
     console.log("Listening");
